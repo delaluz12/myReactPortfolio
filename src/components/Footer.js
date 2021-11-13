9802e4ed637dfdb76f97146ed5f9ff2bcbdcbd0f
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-13 13:52:31 -0600

    sticky footer and nav component drafts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,50 @@
 import React from "react";
+import {
+  MDBFooter,
+  
+} from "mdb-react-ui-kit";
+import { SocialIcon } from "react-social-icons";
 
-function Footer (){
-<div>This is Footer Page</div>
-};
+function Footer() {
+  return (
+    <MDBFooter className="myfooter mt-auto py-3  " style={{backgroundColor: '#e3f2fd'}}>
+      <div className='container'>
+      <div className="pt-2 text-center" >
+        <SocialIcon
+          url="https://github.com/delaluz12"
+          className=""
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35, color: 'lightblue' }}
+        />
+        <SocialIcon
+          url="https://twitter.com/_MariaDeLaLuz_"
+          className=""
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35,  }}
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/mariadllrm/"
+          className=""
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 35, width: 35 }}
+        />
+      </div>
+
+      <div
+        className="text-center p-3"
+        
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a className="text-light" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+      </div>
+    </MDBFooter>
+  );
+}
 
 export default Footer;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,70 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import {SocialIcon} from "react-social-icons";
+
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  
+  MDBCollapse,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 function Navigation() {
+  const [showNav, setShowNav] = useState(false);
   return (
-    <div className="container mx-auto flex justify-between">
-      <nav className="flex">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "inflex-flex items-center py-6 px-3 mr-4 text-red-100  text-4xl font-bold cursive tracking-widest text-white"
-              : "inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-yellow-800 text-4xl font-bold cursive tracking-widest"
-          }
-        >
-          Maria De La Luz
-        </NavLink>
-        <NavLink
-          to="/aboutme"
-          className={({ isActive }) =>
-            isActive
-              ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 "
-              : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          }
-        >
-          About Me
-        </NavLink>
-        <NavLink
-          to="/projects"
-          className={({ isActive }) =>
-            isActive
-              ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 "
-              : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          }
-        >
-          Projects
-        </NavLink>
-        <NavLink
-          to="/resume"
-          className={({ isActive }) =>
-            isActive
-              ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 "
-              : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          }
-        >
-          Resume
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive
-              ? "inline-flex items-center py-3 px-3 my-6 rounded text-red-100 bg-red-700 "
-              : "inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-          }
-        >
-          Contact
-        </NavLink>
-      </nav>
-
-
-      <div className='inline-flex py-3 px-3 my-6'>
-            <SocialIcon url='https://github.com/delaluz12' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}} />
-            <SocialIcon url='https://twitter.com/_MariaDeLaLuz_' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}} />
-            <SocialIcon url='https://www.linkedin.com/in/mariadllrm/' className='mr-4' target='_blank' fgColor='#fff' style={{height: 35, width: 35}} />
-      </div>
-    </div>
+    <>
+      <MDBNavbar expand="lg" className="bg-dark fixed-top">
+        <MDBContainer fluid>
+          <MDBNavbarBrand >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.png"
+              height="30"
+              alt=""
+              loading="lazy"
+            />
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-secondary" : "text-danger"
+              }
+            >
+              Maria De La Luz
+            </NavLink>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler
+            type="button"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setShowNav(!showNav)}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showNav} onClick={() => setShowNav(!showNav)}>
+            <MDBNavbarNav>
+              <MDBNavbarItem>
+                <NavLink
+                  to="/aboutme"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary nav-link"
+                      : "text-danger nav-link"
+                  }
+                >
+                  About Me
+                </NavLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <NavLink
+                  to="/projects"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary nav-link"
+                      : "text-danger nav-link"
+                  }
+                >
+                  Projects
+                </NavLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <NavLink
+                  to="/resume"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary nav-link"
+                      : "text-danger nav-link"
+                  }
+                >
+                  Resume
+                </NavLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary nav-link"
+                      : "text-danger nav-link"
+                  }
+                >
+                  Contact
+                </NavLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
   );
 }
 
