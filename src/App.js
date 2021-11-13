d2b818a35603ea3b1e68e4de5077de539f5a6212
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-13 13:50:02 -0600

    imported home pg comp to app

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Header from "./components/Header";
+import Home from "./components/Home.js";
+// import Footer from "./components/Footer";
 
 // returns the <'comp'/> component to top-level 'App' component
 function App() {
   return (
-    <div>
+    <div className='content'>
+    
       <Header />
-
+      
       {/* iniate the switch which will enable Users to click the links to take them to appropriate page based on what they clicked */}
       <Routes>
         {/* the routes we want Users to be able to navigate to ===Aboutme/Home, Projects, Resume, Contact, */}
         {/* <Route component={About} path='/' exact /> */}
-        <Route element={<About />} path="/" />
+        <Route element={<Home />} path="/" />
         <Route element={<About />} path="aboutme" />
         <Route element={<Projects />} path="projects" />
         <Route element={<Resume />} path="resume" />
         <Route element={<Contact />} path="contact" />
       </Routes>
+      
+    
     </div>
   );
 }
