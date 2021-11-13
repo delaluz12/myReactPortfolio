e8e775140217dcaa86a772859ade8f1e91122a51
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-13 13:48:57 -0600

    added mdbootstrap to project

--- src/index.js
+++ src/index.js
@@ -5,15 +5,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 //import css library
-import './index.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 // importing a component in source folder
 import App from "./App";
+import Footer from "./components/Footer"
+
+// setting the const to what the anonymous fnc returns == the app component in the router wrapper making new component called wrapper
+const Wrapper = () => (<BrowserRouter><App/></BrowserRouter>);
+
+
+//using call back fnc  to pass in the wrapper = wrappedComponent to the withFooter fnc and store the array of components to the array which will be passing in to the render
+const withFooter = WrappedComponent => () => [<WrappedComponent key='1' />, <Footer key='2'/>];
+
 
 // rendering an app component withing the div tag with id=root === think of 'App' as the top-level component
+// setting const to what fnc returns
+const WrapperWithFooter = withFooter(Wrapper);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <WrapperWithFooter/>,
   document.getElementById("root")
 );
