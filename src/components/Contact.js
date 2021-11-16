c967b85d61466f80257a7e10c9c804f93eede685
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-15 23:16:32 -0600

    draft of contact component style -needs fnc

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { MDBInput, MDBContainer, MDBRow, MDBCol, MDBInputGroupText, MDBInputGroup } from "mdb-react-ui-kit";
+
+// import "./contact.css"
 
 function Contact() {
-  return  <div className='topmargin'>This is Contact Page</div>;
+  return (
+    <MDBContainer>
+      <div className="topmargin text-center">Let's Connect!</div>
+      <MDBRow className='m-4'>
+        <MDBCol>
+          <form>
+           
+          
+            <MDBInput label="Na" id="typeText" type="text" className='m-2'  />
+           
+            <MDBInput label="Email input" id="typeEmail" type="email" className='m-2' />
+            <MDBInput label="Message" id="textAreaExample" textarea rows={4} className='m-2' />
+          </form>
+          <div className="text-center">reach me at c12vida@gmail.com</div>
+        </MDBCol>
+        
+      </MDBRow>
+      
+    </MDBContainer>
+  );
 }
 
 export default Contact;
